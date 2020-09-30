6ea2a8c10dd8deb3d549a2ad242e66ad19363cd5
jasmaa <jasonmaa3955@gmail.com>
2020-09-30 13:56:03 -0400

    Add event pagination and sorting

--- src/components/Events/index.js
+++ src/components/Events/index.js
@@ -1,57 +1,89 @@
-import React from 'react';
-import { Container } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { Container, TablePagination, Select, Box, Grid } from '@material-ui/core';
 import EventCard from './EventCard';
-import placeholder from './placeholder.jpg';
 
-const dummyEvents = [
-    {
-        title: 'Event 1',
-        imageURI: placeholder,
-        date: new Date('2020-09-30'),
-        location: '221B Baker Street',
-        link: 'https://google.com',
-        content: 'This is an event',
-    },
-    {
-        title: 'Event 2',
-        imageURI: placeholder,
-        date: new Date('2020-09-10'),
-        location: 'Online',
-        link: 'https://google.com',
-        content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Quam vulputate dignissim suspendisse in est. At lectus urna duis convallis
-        convallis tellus id interdum. Neque volutpat ac tincidunt vitae. Ipsum faucibus vitae aliquet nec ullamcorper
-        sit amet risus nullam. Nullam ac tortor vitae purus faucibus. Faucibus purus in massa tempor. Proin fermentum leo
-        vel orci porta non pulvinar neque laoreet. Viverra adipiscing at in tellus. Morbi tristique senectus et netus et
-        malesuada fames ac. Enim nec dui nunc mattis. Pellentesque id nibh tortor id aliquet lectus proin nibh. Accumsan
-        lacus vel facilisis volutpat est velit egestas dui. Vitae semper quis lectus nulla at. Cursus turpis massa tincidunt
-        dui ut ornare lectus. Non nisi est sit amet facilisis.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Quam vulputate dignissim suspendisse in est. At lectus urna duis convallis
-        convallis tellus id interdum. Neque volutpat ac tincidunt vitae. Ipsum faucibus vitae aliquet nec ullamcorper
-        sit amet risus nullam. Nullam ac tortor vitae purus faucibus. Faucibus purus in massa tempor. Proin fermentum leo
-        vel orci porta non pulvinar neque laoreet. Viverra adipiscing at in tellus. Morbi tristique senectus et netus et
-        malesuada fames ac. Enim nec dui nunc mattis. Pellentesque id nibh tortor id aliquet lectus proin nibh. Accumsan
-        lacus vel facilisis volutpat est velit egestas dui. Vitae semper quis lectus nulla at. Cursus turpis massa tincidunt
-        dui ut ornare lectus. Non nisi est sit amet facilisis.`,
-    },
-    {
-        title: 'Event 3',
-        date: new Date('2020-01-03'),
-        link: 'https://google.com',
-        content: 'This is also an event',
-    },
-];
+import { dummyEvents } from './dummyData';
+
+const rowsPerPage = 2;
 
 /**
  * Events page
  */
 const Events = () => {
+
+    const [events, setEvents] = useState([]);
+    const [page, setPage] = useState(0);
+    const [sortingOption, setSortingOption] = useState('')
+
+    useEffect(() => {
+        // TODO: receive events from CMS
+        dummyEvents.sort((e1, e2) => e1.date < e2.date);
+        setEvents(dummyEvents);
+    }, []);
+
+    /**
+     * Sorts events by option
+     * @param {*} option 
+     */
+    const sortEvents = (option) => {
+        switch (option) {
+            case 'latest':
+                // Sort by most recent event
+                setEvents(prev => {
+                    prev.sort((e1, e2) => e1.date < e2.date);
+                    return prev;
+                });
+                setSortingOption('latest');
+                break;
+            case 'alphabet':
+                // Sort by alphabetical
+                setEvents(prev => {
+                    prev.sort((e1, e2) => e1.title > e2.title);
+                    return prev;
+                });
+                setSortingOption('alphabet');
+                break;
+            default:
+                // no-op
+                break;
+        }
+    }
+
     return (
         <Container>
             <h1>Upcoming Events</h1>
-            {dummyEvents.map(event => <EventCard event={event} />)}
-        </Container>
+            <Grid container direction="row">
+                <Grid container item justify="flex-end" lg={10}>
+                    <Box display="flex" alignItems="center">
+                        <Box mr={2}>
+                            <span>Sort by:</span>
+                        </Box>
+                        <Select
+                            native
+                            value={sortingOption}
+                            onChange={e => sortEvents(e.target.value)}
+                        >
+                            <option value="latest">Latest</option>
+                            <option value="alphabet">Alphabetical</option>
+                        </Select>
+                    </Box>
+                </Grid>
+                <Grid container item justify="flex-end" lg={2}>
+                    <TablePagination
+                        component="div"
+                        count={events.length}
+                        page={page}
+                        onChangePage={(_, page) => setPage(page)}
+                        rowsPerPage={rowsPerPage}
+                        rowsPerPageOptions={[rowsPerPage]}
+                    />
+                </Grid>
+            </Grid>
+            {
+                events.slice(page * rowsPerPage, (page + 1) * rowsPerPage)
+                    .map(event => <EventCard event={event} />)
+            }
+        </Container >
     );
 }
 
