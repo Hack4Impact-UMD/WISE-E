2ae23e949fa0e86abb3408fe2fe3f38791842cd7
jasmaa <jasonmaa3955@gmail.com>
2020-09-30 12:43:41 -0400

    Improve event card css to be mobile compatible

--- src/components/Events/EventCard/index.js
+++ src/components/Events/EventCard/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Card, CardContent, Box } from '@material-ui/core';
+import { Card, CardContent, Box, Grid, Link } from '@material-ui/core';
+import noImage from './noImage.png';
 import styles from './styles.module.css';
-import placeholder from './placeholder.jpg';
 
 /**
  * Info card for a single event
@@ -11,19 +11,25 @@ import placeholder from './placeholder.jpg';
 const EventCard = ({ event }) => {
     return (
         <Box m={4}>
-            <Card variant="outlined">
-                <Box display="flex">
-                    <Box flexShrink={0} className={styles['card-img-container']}>
-                        <img className={styles['card-img']} src={placeholder} />
-                    </Box>
-                    <CardContent className={styles['card-content']}>
-                        <Box>
-                            <h2>{event.title}</h2>
-                            <em>{event.date.toDateString()}</em>
-                            <p>{event.content}</p>
-                        </Box>
-                    </CardContent>
-                </Box>
+            <Card variant="outlined" className={styles['card']}>
+                <Grid container direction="row">
+                    <Grid container item md={3}>
+                        <img className={styles['card-img']} src={event.imageURI ? event.imageURI : noImage} />
+                    </Grid>
+                    <Grid container item md={9}>
+                        <CardContent className={styles['card-content']}>
+                            <Box>
+                                <h2>
+                                    <Link href={event.link}>{event.title}</Link>
+                                </h2>
+                                <span>
+                                    <em>{event.date.toDateString()}{event.location ? ` - ${event.location}` : ''}</em>
+                                </span>
+                                <p>{event.content}</p>
+                            </Box>
+                        </CardContent>
+                    </Grid>
+                </Grid>
             </Card>
         </Box>
     );
--- src/components/Events/index.js
+++ src/components/Events/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
 import EventCard from './EventCard';
+import placeholder from './placeholder.jpg';
 
-const placeholderEvents = [
+const dummyEvents = [
     {
         title: 'Event 1',
+        imageURI: placeholder,
         date: new Date('2020-09-30'),
+        location: '221B Baker Street',
+        link: 'https://google.com',
         content: 'This is an event',
     },
     {
         title: 'Event 2',
+        imageURI: placeholder,
         date: new Date('2020-09-10'),
+        location: 'Online',
+        link: 'https://google.com',
         content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Quam vulputate dignissim suspendisse in est. At lectus urna duis convallis
+        convallis tellus id interdum. Neque volutpat ac tincidunt vitae. Ipsum faucibus vitae aliquet nec ullamcorper
+        sit amet risus nullam. Nullam ac tortor vitae purus faucibus. Faucibus purus in massa tempor. Proin fermentum leo
+        vel orci porta non pulvinar neque laoreet. Viverra adipiscing at in tellus. Morbi tristique senectus et netus et
+        malesuada fames ac. Enim nec dui nunc mattis. Pellentesque id nibh tortor id aliquet lectus proin nibh. Accumsan
+        lacus vel facilisis volutpat est velit egestas dui. Vitae semper quis lectus nulla at. Cursus turpis massa tincidunt
+        dui ut ornare lectus. Non nisi est sit amet facilisis.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
         labore et dolore magna aliqua. Quam vulputate dignissim suspendisse in est. At lectus urna duis convallis
         convallis tellus id interdum. Neque volutpat ac tincidunt vitae. Ipsum faucibus vitae aliquet nec ullamcorper
         sit amet risus nullam. Nullam ac tortor vitae purus faucibus. Faucibus purus in massa tempor. Proin fermentum leo
@@ -23,6 +38,7 @@ const placeholderEvents = [
     {
         title: 'Event 3',
         date: new Date('2020-01-03'),
+        link: 'https://google.com',
         content: 'This is also an event',
     },
 ];
@@ -34,7 +50,7 @@ const Events = () => {
     return (
         <Container>
             <h1>Upcoming Events</h1>
-            {placeholderEvents.map(event => <EventCard event={event} />)}
+            {dummyEvents.map(event => <EventCard event={event} />)}
         </Container>
     );
 }
