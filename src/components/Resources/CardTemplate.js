af4c4fa755f0c38d89345039f5a9a095d9c13861
Codingdn <danthedevnguyen@gmail.com>
2020-10-08 18:28:36 -0400

    updated css to makeStyles; added margins to cards

--- src/components/Resources/CardTemplate.js
+++ src/components/Resources/CardTemplate.js
@@ -3,14 +3,15 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import "./Card.css";
 
 const useStyles = makeStyles({
   root: {
     minWidth: 100,
+    marginRight: 20
   },
   title: {
     fontSize: 14,
+    textAlign: "center"
   },
   pos: {
     marginBottom: 12,
