d4e587e768ac8787046b9094cdf4ed232e387184
Codingdn <danthedevnguyen@gmail.com>
2020-10-24 17:39:45 -0400

    updated template with cards

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,12 +1,57 @@
-import React from 'react';
-import { Container } from '@material-ui/core';
+import React from "react";
+import { Container, makeStyles } from "@material-ui/core";
+import msg from "./sampleData.json";
+import CardTemplate from "./CardTemplate";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles({
+  title: {
+    textAlign: "center",
+  },
+  list: {
+    listStyleType: "none",
+  },
+});
+
+const dummyAffiliated = [
+  { name: "Spencer" },
+  { name: "Katherine" },
+  { name: "Daisy" },
+  { name: "Paul" },
+  { name: "Abby" },
+];
 
 const Team = () => {
-    return (
-        <Container>
-            <h1>Our Team</h1>
-        </Container>
-    );
-}
+  const classes = useStyles();
+  return (
+    <Container>
+      <h1 className={classes.title}>Meet Our Team</h1>
+      <br />
+      <CardTemplate
+        name={msg.Person1.name}
+        imageURL={msg.Person1.imageURL}
+        description={msg.Person1.description}
+        position={msg.Person1.position}
+        alt={msg.Person1.alt}
+        email={msg.Person1.email}
+      />
+
+      {/**Still need to figure out how to make these name apepar in a grid */}
+      <h2>Others Involved</h2>
+      <Container>
+        <Grid contanier spacing={3}>
+          {dummyAffiliated.map((item) => {
+            return (
+              <Grid item md={4}>
+                  {/**might make this a card to add hover effect */}
+                <div>{item.name}</div>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Container>
+    </Container>
+  );
+};
 
 export default Team;
