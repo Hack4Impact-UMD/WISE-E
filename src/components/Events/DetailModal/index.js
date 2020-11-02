1cfc899dfa418482cb966ab0f619b03367f5d9b5
jasmaa <jasonmaa3955@gmail.com>
2020-11-02 16:28:40 -0500

    Change event modal to dialog for scrolling

--- src/components/Events/DetailModal/index.js
+++ src/components/Events/DetailModal/index.js
@@ -1,49 +1,42 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Container, Box, Modal, Card, CardContent, IconButton } from '@material-ui/core';
+import { Container, Box, Dialog, IconButton } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 import TimeDisplay from '../TimeDisplay';
 import LocationDisplay from '../LocationDisplay';
 
-const useStyles = makeStyles({
-  card: {
-    width: '100%',
-  }
-});
-
 export default function DetailModal({ event, open, onClose }) {
-
-  const classes = useStyles();
 
   const { title, startTime, endTime, location, content } = event;
 
   return (
-    <Modal
+    <Dialog
       open={open}
       onClose={onClose}
+      scroll="body"
+      BackdropComponent={Backdrop}
+      BackdropProps={{ timeout: 300 }}
     >
-      <Container maxWidth="md">
-        <Box display="flex" justifyContent="center" mt={5}>
-          <Card className={classes.card}>
-            <CardContent>
+      <Fade in={open}>
+        <Container maxWidth="md">
+          <Box display="flex" flexDirection="column" justifyContent="center" pt={3} pb={5} px={3}>
+            <Box display="flex" justifyContent="end">
+              <IconButton onClick={onClose}>
+                <CloseIcon />
+              </IconButton>
+            </Box>
 
-              <Box display="flex" justifyContent="end">
-                <IconButton onClick={onClose}>
-                  <CloseIcon />
-                </IconButton>
-              </Box>
+            <h1>{title}</h1>
 
-              <h1>{title}</h1>
+            <LocationDisplay location={location} />
+            <TimeDisplay startTime={startTime} endTime={endTime} />
 
-              <LocationDisplay location={location} />
-              <TimeDisplay startTime={startTime} endTime={endTime} />
-
-              {/* TODO: Assuming paragraph content for now. Will be replaced with Contentful rich text renderer. */}
-              <p>{content}</p>
-            </CardContent>
-          </Card>
-        </Box>
-      </Container>
-    </Modal>
+            {/* TODO: Assuming paragraph content for now. Will be replaced with Contentful rich text renderer. */}
+            <p>{content}</p>
+          </Box>
+        </Container>
+      </Fade>
+    </Dialog>
   );
 }
