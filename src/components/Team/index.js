c45ee137cc9b276004455ea2b28727acae3f966a
Codingdn <danthedevnguyen@gmail.com>
2020-11-14 22:37:45 -0500

    add more sample data; fixed some css

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -16,9 +16,19 @@ const useStyles = makeStyles({
   cardContent: {
     margin: "15px",
   },
+  sideBySide: {
+    display: "flex",
+  },
+  teamPicture: {
+    width: "650px",
+    margin: "10px",
+  },
+  Card: {
+    height: "150px",
+  }
 });
 
-const dummyAffiliated = [
+const affiliatedNames = [
   {
     name: "Dr. Camellia Okpodu",
     position: "Dean of the School of Arts and Science at Xavier University",
@@ -39,31 +49,57 @@ const dummyAffiliated = [
   },
 ];
 
+const sampleTeamPic =
+  "https://cdn.vox-cdn.com/thumbor/Ndb49Uk3hjiquS041NDD0tPDPAs=/0x169:1423x914/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/7342855/microsoftteams.0.jpg";
+
 const Team = () => {
   const classes = useStyles();
   return (
     <Container>
-      <h1 className={classes.title}>Meet Our Team</h1>
-      <br />
-      <CardTemplate
-        name={msg.Person1.name}
-        imageURL={msg.Person1.imageURL}
-        description={msg.Person1.description}
-        position={msg.Person1.position}
-        alt={msg.Person1.alt}
-        email={msg.Person1.email}
-      />
+      <div>
+        <div className={classes.sideBySide}>
+          <div>
+            <h1 className={classes.title}>Meet Our Team</h1>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+          </div>
+          <img src={sampleTeamPic} alt="team" className={classes.teamPicture} />
+        </div>
+      </div>
 
-      <center><h2>Others Involved</h2></center>
+      <br />
+
+      {/**To make carousel, would probably need to install another dependency */}
+      {msg.samplePeople.map((person) => (
+        <CardTemplate
+          name={person.name}
+          imageURL={person.imageURL}
+          description={person.description}
+          position={person.position}
+          alt={person.alt}
+          email={person.email}
+        />
+      ))}
+
+      <br />
+      <center>
+        <h2>Others Involved</h2>
+      </center>
       <Container>
         <Grid container spacing={3}>
-          {dummyAffiliated.map((item) => {
+          {affiliatedNames.map((item) => {
             return (
               <Grid item md={4}>
-                {/**might make this a card to add hover effect */}
-                <Card class>
+                <Card className={classes.Card}>
                   <CardContent className={classes.cardContent}>
-                    <h2>{item.name}</h2>
+                    <h3>{item.name}</h3>
                     <p>{item.position}</p>
                   </CardContent>
                 </Card>
