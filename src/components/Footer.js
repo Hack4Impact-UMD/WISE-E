f58eed2cc416a796f093de8d5cf3e41783a58ccc
elizabeth-chen <44928715+elizabeth-chen@users.noreply.github.com>
2020-11-27 18:24:22 -0500

    removed import

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
-import '../index.css';
 
 function Copyright() {
   return (
