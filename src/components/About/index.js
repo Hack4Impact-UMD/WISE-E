8bb7e57d95742038138a4cd5298153e9d4d70d58
Lydia Hu <lh7560@gmail.com>
2021-01-17 19:28:53 -0500

    Update About page header

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import { Container, makeStyles } from '@material-ui/core';
-import earth from './earth.jpg'
-
-const useStyles = makeStyles({
-    headerImage: {
-        width: "650px",
-        margin:"10px"
-    }
-})
+import React from "react";
+import { Container, makeStyles } from "@material-ui/core";
+import styles from "./styles.module.css";
 
 const About = () => {
-    const classes = useStyles(); 
 
-    return (
-        <Container>
-            {/* <img src={require('./earth.jpg')} alt="earth image"/> */}
-            <img src={earth} alt="earth image" className={classes.headerImage}/>
-            <h1>About</h1>
-            <p>The Women in Science, Energy, and the Environment (WISE.E) initiative is a program of the Center for Sustainable Communities (CSC). WISE.E is based in Georgia and has a national footprint of educational programs whose mission is to recruit, engage, and sustain women and girls in STEM fields. By connecting women to professional networks, grant opportunities, and events where they can meet like-minded professionals, WISE.E aims to support a historically under resourced population in these industries and encourage involvement.
-            </p>
-        </Container>
-    );
-}
+  return (
+    <>
+      <div className={styles["banner-img"]}>
+        <br></br>
+        <br></br>
+
+        <div className={styles["header-div-1"]}>
+          <h1 className={styles["header-text"]}>Recruit. &nbsp; </h1>
+        </div>
+
+        <div className={styles["header-div-2"]}>
+          <h1 className={styles["header-text"]}>Engage. &nbsp; </h1>
+        </div>
+
+        <div className={styles["header-div-3"]}>
+          <h1 className={styles["header-text"]}>Sustain. &nbsp; </h1>
+        </div>
+      </div>
+
+      <Container>
+
+        <h1>About</h1>
+        <p>
+          The Women in Science, Energy, and the Environment (WISE.E) initiative
+          is a program of the Center for Sustainable Communities (CSC). WISE.E
+          is based in Georgia and has a national footprint of educational
+          programs whose mission is to recruit, engage, and sustain women and
+          girls in STEM fields. By connecting women to professional networks,
+          grant opportunities, and events where they can meet like-minded
+          professionals, WISE.E aims to support a historically under resourced
+          population in these industries and encourage involvement.
+        </p>
+      </Container>
+    </>
+  );
+};
 
 export default About;
