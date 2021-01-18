08b942153f4db1c3ddc913fb876a31cbf70fd5be
Lydia Hu <lh7560@gmail.com>
2021-01-17 22:17:59 -0500

    Update Resources and footer
    -add headers to Resources page
    -update footer with icons

--- src/components/Resources/index.js
+++ src/components/Resources/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import { Container } from '@material-ui/core';
+import React from "react";
+import { Container } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
 
 const Resources = () => {
-    return (
-        <Container>
-            <h1>Resources</h1>
-            <p>Resources list</p>
-        </Container>
-    );
-}
+  return (
+    <Container>
+      <h1>Resources</h1>
+      <Typography><h2>Leadership</h2></Typography>
+      <Typography><h2>Financial</h2></Typography>
+      <Typography><h2>Career Pathway</h2></Typography>
+      <Typography><h2>Awards</h2></Typography>
+      <Typography><h2>Academic</h2></Typography>
+      <Typography><h2>Internships</h2></Typography>
+      <Typography><h2>Fellowships</h2></Typography>
+    </Container>
+  );
+};
 
 export default Resources;
